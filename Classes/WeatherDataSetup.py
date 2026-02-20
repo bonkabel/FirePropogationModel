832acb02b52532322e8ea085f4f7107851d35568
Abel <abel.amy0@gmail.com>
2026-02-20 09:47:37 -0500

    Add comments

--- Classes/WeatherDataSetup.py
+++ Classes/WeatherDataSetup.py
@@ -6,24 +6,35 @@ from retry_requests import retry
 from scipy.ndimage import zoom
 
 class WeatherDataSetup:
-    def __init__(self, southLat, westLon, gridSize, cellResolution, modelResolution, cache):
+    def __init__(self, southLat, westLon, gridSize, cellResolution, coarseResolution, cache):
+        """
+        Creates a geographic weather grid, gets coarse weather data from open-meteo, and interpolates it to a finer resolution
+
+        :param southLat: latitude of the bottom edge of the grid in degrees, southern boundary of the area
+        :param westLon: longitude of the left edge of the grid in degrees, western boundary of the area
+        :param gridSize: number of cells per side of the final fine resolution grid. Resulting grid will have the resolution (gridSize x gridSize)
+        :param cellResolution: Size of each fine grid cell in kilometres. Ex: 1.0 would mean a grid cell represents 1 km x 1 km
+        :param coarseResolution: Resolution of the coarse grid before interpolation. Determines what is retrieved from the API
+        :param cache: Boolean value indicating whether API request caching and retry logic should be enabled
+        """
+
         self.southLat = southLat
         self.westLon = westLon
 
         self.gridSize = gridSize
         self.cellResolution = cellResolution
-        self.modelResolution = modelResolution
+        self.modelResolution = coarseResolution
 
         self.cache = cache
 
         self.latStep = cellResolution / 111
         self.lonStep = cellResolution / (111 * math.cos(math.radians(southLat)))
 
-        scale = modelResolution / cellResolution
+        scale = coarseResolution / cellResolution
         self.coarseSize = int(gridSize / scale)
 
-        self.latStepCoarse = modelResolution / 111
-        self.lonStepCoarse = modelResolution / (111 * math.cos(math.radians(southLat)))
+        self.latStepCoarse = coarseResolution / 111
+        self.lonStepCoarse = coarseResolution / (111 * math.cos(math.radians(southLat)))
 
         self.coarsePoints = self._GenerateCoarseGridPoints()
         self.gridPoints = self._GenerateGridPoints()
@@ -31,6 +42,11 @@ class WeatherDataSetup:
         self.client = self._CreateClient()
 
     def _GenerateCoarseGridPoints(self):
+        """
+        Generates latitude and longitude coordinate pairs for each cell in the coarse resolution grid
+
+        :return: List of (latitude, longitude) tuples representing each coarse grid cell location
+        """
         points = []
 
         for i in range(self.coarseSize):
@@ -42,6 +58,11 @@ class WeatherDataSetup:
         return points
 
     def _GenerateGridPoints(self):
+        """
+        Generates latitude and longitude coordinate pairs for each cell in the fine resolution grid
+
+        :return: List of (latitude, longitude) tuples representing each fine grid cell location
+        """
         points = []
 
         for i in range(self.gridSize):
@@ -53,6 +74,13 @@ class WeatherDataSetup:
         return points
 
     def _CreateClient(self):
+        """
+        Creates and returns on open-meteo client
+
+        If caching is enbled, wraps the session with cache and retry logic to reduce redundant requests and handle failures
+
+        :return: Configured openmeteo_requests.Client instance
+        """
         if self.cache:
             cacheSession = requests_cache.CachedSession('.cache', expire_after=3600)
             retrySession = retry(cacheSession, retries=8, backoff_factor=1)
@@ -62,6 +90,14 @@ class WeatherDataSetup:
 
 
     def _Upscaledata(self, coarseData):
+        """
+        Interpolates coarse grid weather data to match the fine grid size
+
+        Use bilinear interpolation to scale each 2D weather array
+
+        :param coarseData: Dictionary mapping weather variable names to 2D NumPy arrays at coarse resolution.
+        :return: Dictionary mapping weather variable names to @d NumPy arrays at fine resolution
+        """
         scaleFactor = self.gridSize / self.coarseSize
 
         fineData = {}
@@ -72,6 +108,13 @@ class WeatherDataSetup:
         return fineData
 
     def CreateGrid(self):
+        """
+        Fetches weather data for all coarse grid points in batches.
+        Stores the results in 2D arrays.
+        Interpolates them to a finer resolution
+
+        :return: Dictionary containing fine resolution 2D NumPy arrays for reach weather variable
+        """
         batchSize = 125
 
         coarseData = {
