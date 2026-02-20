fd3ca4828fcc77bd187a86174bae926906fbc672
Abel <abel.amy0@gmail.com>
2026-02-20 09:48:07 -0500

    Add comments

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from Classes.WeatherDataSetup import WeatherDataSetup
 
+# Just for testing purposes
+
 if __name__ == '__main__':
     weatherSetup = WeatherDataSetup(42.817816, -80.633052, 100, 2, 10, True)
     grid = weatherSetup.CreateGrid()
