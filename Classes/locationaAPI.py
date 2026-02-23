d7f65aefdf7808c2486b77811543367c266518ca
Amanjot Kaur Aulakh <amanjotkaur23101998@gmail.com>
2026-02-23 18:04:53 -0500

    Longitude and latitude of a city

--- Classes/locationaAPI.py
+++ Classes/locationaAPI.py
@@ -4,7 +4,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 api_key = os.getenv("API_KEY")
-city = input("What is the name of the city?")
-country = input("What is the name of the country")
-name = requests.get(f"https://geocode.maps.co/search?q={city,country}&api_key={api_key}")
-print(name)
+# city = input("What is the name of the city?")
+# country = input("What is the name of the country")
+city = "Miami"
+country = "FL"
+name = requests.get(f"https://geocode.maps.co/search?q={city},{country}&api_key={api_key}")
+data = name.json()
+lat = data[0].get('lat')
+lon = data[0].get('lon')
