0a4bb48ec40c634ff244fea6ab965345830fb9e3
Amanjot Kaur Aulakh <amanjotkaur23101998@gmail.com>
2026-02-23 18:14:14 -0500

    Organized with class

--- Classes/locationaAPI.py
+++ Classes/locationaAPI.py
@@ -1,14 +1,18 @@
 import os
 import requests
 from dotenv import load_dotenv
-
 load_dotenv()
-api_key = os.getenv("API_KEY")
-# city = input("What is the name of the city?")
-# country = input("What is the name of the country")
-city = "Miami"
-country = "FL"
-name = requests.get(f"https://geocode.maps.co/search?q={city},{country}&api_key={api_key}")
-data = name.json()
-lat = data[0].get('lat')
-lon = data[0].get('lon')
+
+class Location():
+    def __init__(self, city, country):
+
+        self.api_key = os.getenv("API_KEY")
+        self.city = city
+        self.country = country
+
+    def coordinates(self):
+        name = requests.get(f"https://geocode.maps.co/search?q={self.city},{self.country}&api_key={self.api_key}")
+        data = name.json()
+        lat = data[0].get('lat')
+        lon = data[0].get('lon')
+        return lat, lon
