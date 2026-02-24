8e7b4fa12a9761963fdd3027a5bf4fcc4baaf860
Amanjot Kaur Aulakh <amanjotkaur23101998@gmail.com>
2026-02-24 05:55:52 -0500

    Corrected the grid alignment

--- Classes/Map_grid.py
+++ Classes/Map_grid.py
@@ -11,29 +11,37 @@ import numpy as np
 import folium
 import branca.colormap as cm
 import random
+from locationaAPI import Location
 
 class FireGrid():
-    def __init__(self, south, north, west, east, rows=50, cols=50):
+    def __init__(self, south, north, west, east, rows=10, cols=10):
         self.south = south
         self.north = north
         self.west = west
         self.east = east
         self.rows = rows
         self.cols = cols
+        self.location = Location("Brampton", "Canada")
+        self.lat, self.lon = self.location.coordinates()
 
-        self.lat_step = (north - south) / rows
-        self.lon_step = (east - west) / cols
+        self.north = float(self.lat) + 0.1
+        self.south = float(self.lat) - 0.1
+        self.east = float(self.lon) + 0.2
+        self.west = float(self.lon) - 0.2
+
+        self.lat_step = (self.north - self.south) / rows
+        self.lon_step = (self.east - self.west) / cols
 
         # Fire affinity values (0–1)
         self.affinity = np.random.rand(rows, cols)
 
     def show_map(self):
         # Deciphering the center of the map
-        center_lat = (self.south + self.north) / 2
-        center_lon = (self.west + self.east) / 2
+        center_lat = self.lat
+        center_lon = self.lon
 
         # Shows  map centring with the location along with the zoom level at start
-        m = folium.Map(location=[center_lat, center_lon], zoom_start=14)
+        m = folium.Map(location=[center_lat, center_lon], zoom_start=12)
 
         # Color gradient for grid ranging from 0 to 1
         colormap = cm.linear.YlOrRd_09.scale(0, 1)
@@ -47,11 +55,11 @@ class FireGrid():
 
                 # Bounds for the grid
                 bounds = [
-                    [self.west + j*self.lon_step,
-                     self.south + i*self.lat_step],
+                    [self.south + i*self.lat_step,
+                     self.west + j*self.lon_step],
 
-                    [self.west + (j+1)*self.lon_step,
-                     self.south + (i+1)*self.lat_step]
+                    [self.south + (i+1)*self.lat_step,
+                     self.west + (j+1)*self.lon_step]
                 ]
 
                 # Creating grid rectangle on top of the map
@@ -71,7 +79,7 @@ class FireGrid():
         m.save("full_grid_map.html")
         print("Saved as full_grid_map.html")
         # Opening it on the browser
-        webbrowser.open("file://" + os.path.realpath("fire_rectangles.html"))
+        webbrowser.open("file://" + os.path.realpath("full_grid_map.html"))
 
 # Values for map location
 grid = FireGrid(
@@ -79,8 +87,8 @@ grid = FireGrid(
     north=23.90,
     west=90.35,
     east=90.50,
-    rows=60,
-    cols=60
+    rows=10,
+    cols=10
 )
 
 grid.show_map()
