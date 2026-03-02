6620c9e77b224f34ec1ca39bb02c65ac4641561d
Abel <abel.amy0@gmail.com>
2026-03-02 01:42:36 -0500

    Add Ignite method

--- Classes/Simulation.py
+++ Classes/Simulation.py
@@ -1,5 +1,19 @@
+from Classes.Grid import Grid
+import numpy as np
+
 class Simulation:
+    """
+    Probabilistic fire spread.
+    Deterministic extinction.
+    """
+
+
     def __init__(self, grid, dt):
+        """
+
+        :param grid: A Grid object containing weather and terrain data
+        :param dt: Delta time. The change in time for a step in the simulation
+        """
         self.grid = grid
         self.dt = dt
         self.time = 0.0
@@ -13,5 +27,10 @@ class Simulation:
     def _Calculate(self):
         pass
 
+
+
     def _Update(self):
         pass
+
+    def Ignite(self, x, y):
+        self.grid.state[x,y] = Grid.BURNING
