d43c40088faba4b111c6b88ab1f251794a23cb70
Abel <abel.amy0@gmail.com>
2026-03-11 11:54:41 -0400

    Update main

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import numpy as np
+
+from Classes.Fire_Spread import Fire_Spread
 from Classes.Grid import Grid
+from Classes.Simulation import Simulation
 # from Classes.Map_grid import FireGrid
 from Classes.TerrainDataSetup import TerrainDataSetup
+from Classes.Visualization import Visualization
 from Classes.WeatherDataSetup import WeatherDataSetup
 
 # Just for testing purposes
@@ -8,33 +13,57 @@ from Classes.WeatherDataSetup import WeatherDataSetup
 if __name__ == '__main__':
 
     # Testing data
-    southLat = 42.817816 # latitude
-    westLon = -80.633052 # longitude
-    gridSize = 100 #km
+    southLat = -9.248872 # latitude
+    westLon = -45.917065 # longitude
+    gridSize = 100 #Dimensions
     cellResolution = 2 #km
     coarseResolution = 10 #km
     cache = True
 
-    # # Testing Map_grid
-    # fireGrid = FireGrid(rows=60,cols=60)
-    # fireGrid.show_map()
-
-    # Testing WeatherDataSetup
-    weatherSetup = WeatherDataSetup(42.817816, -80.633052, 100, 2, 10, True, False, True)
+    # WeatherDataSetup
+    weatherSetup = WeatherDataSetup(42.817816, -80.633052, 100, 2, 10, True, True, True)
     weatherLayers = weatherSetup.CreateWeatherLayers()
-    print(weatherLayers["wind_direction"][0])
-    with open("output.txt", 'w') as f:
-        f.write(str(weatherLayers["wind_direction"][0]))
-    
-    
     print("Weather grid data done")
 
-    # Testing TerrainDataSetup
-    # terrainSetup = TerrainDataSetup(southLat, westLon, gridSize, cellResolution)
-    # terrainLayers = terrainSetup.CreateTerrainLayers()
-    # print("Terrain grid data done")
+    # TerrainDataSetup
+    terrainSetup = TerrainDataSetup(southLat, westLon, gridSize, cellResolution)
+    terrainLayers = terrainSetup.CreateTerrainLayers()
+    print("Terrain grid data done")
 
-    # Testing Grid
-    # grid = Grid(weatherLayers, terrainLayers, gridSize)
-    # print("Grid setup")
+    # ROS calculation
+    fireSpread = Fire_Spread(weatherLayers['humidity'], weatherLayers['wind_speed'], weatherLayers['precipitation'], weatherLayers['temperature'], terrainLayers['trees'])
+    rateOfSpread = fireSpread.roscalculation()
+
+    # Grid
+    grid = Grid(gridSize, cellResolution, weatherLayers, terrainLayers, rateOfSpread)
+    print("Grid setup done")
+
+    # Simulation
+    simulation = Simulation(grid, 10, 200)
+    simulation.IgniteRandom(10)
+    simulation.Run()
+    print("Simulation done")
+
+    # Visualization
+    viz = Visualization(
+        simulation,
+        southLat=terrainSetup.southLat,
+        westLon=terrainSetup.westLon,
+        northLat=terrainSetup.northLat,
+        eastLon=terrainSetup.eastLon
+    )
+    viz.saveTimeline()
+    print("Visualization done")
+
+
+
+
+
+
+
+
+
+
+
+
 
